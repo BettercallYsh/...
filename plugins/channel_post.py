e692af37e12ab2efd5609b04546327ddbb878cca
BettercallYsh <166407864+BettercallYsh@users.noreply.github.com>
2024-06-08 00:04:45 +0530

    Create channel_post.py

---
+++ plugins/channel_post.py
@@ -0,0 +1,63 @@
+import asyncio
+from pyrogram import filters, Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import FloodWait
+
+from bot import Bot
+from config import CHANNEL_ID, DISABLE_CHANNEL_BUTTON
+from help_func import encode
+from database import is_admin
+
+@Bot.on_message(filters.private & filters.user() & ~filters.command(['start', 'users', 'broadcast', 'batch', 'genlink', 'stats']))
+async def channel_post(client: Client, message: Message):
+    if not await is_admin(message.from_user.id):
+        await message.reply_text("You don't have permission to use this command.")
+        return
+
+    reply_text = await message.reply_text("Please Wait...!", quote=True)
+    try:
+        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
+    except Exception as e:
+        print(e)
+        await reply_text.edit_text("Something went Wrong..!")
+        return
+
+    converted_id = post_message.id * abs(client.db_channel.id)
+    string = f"get-{converted_id}"
+    base64_string = await encode(string)
+    link = f"https://t.me/{client.username}?start={base64_string}"
+
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+
+    await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview=True)
+
+    if not DISABLE_CHANNEL_BUTTON:
+        try:
+            await post_message.edit_reply_markup(reply_markup)
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+            await post_message.edit_reply_markup(reply_markup)
+        except Exception:
+            pass
+
+@Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
+async def new_post(client: Client, message: Message):
+    if DISABLE_CHANNEL_BUTTON:
+        return
+
+    converted_id = message.id * abs(client.db_channel.id)
+    string = f"get-{converted_id}"
+    base64_string = await encode(string)
+    link = f"https://t.me/{client.username}?start={base64_string}"
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+
+    try:
+        await message.edit_reply_markup(reply_markup)
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        await message.edit_reply_markup(reply_markup)
+    except Exception:
+        pass
